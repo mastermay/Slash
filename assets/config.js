62aa1c17bc97a7f277011f48303b517a67f18f80
mastermay <986492616@qq.com>
2017-12-27 17:54:07 +0800

    移除多说

--- assets/config.js
+++ assets/config.js
@@ -1,12 +1,7 @@
 config = {
-    blogname: 'Slash',
-    sep: ' | ',
-    user: 'mastermay',
-    repo: 'Slash',
-    per_page: 1,
-    duoshuo_id: 'slash'
-}
-
-var duoshuoQuery = {
-    short_name: config['duoshuo_id']
+  blogname: 'Slash',
+  sep: ' | ',
+  user: 'mastermay',
+  repo: 'Slash',
+  per_page: 1
 };
