18b4867862020b877fcce52de64bb518b6402018
mastermay <986492616@qq.com>
2020-05-12 14:56:22 +0800

    chroe: webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -13,10 +13,6 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist'
-  },
   plugins: [
     new CleanWebpackPlugin({ cleanAfterEveryBuildPatterns: ['dist'] }),
     new HtmlWebpackPlugin({
